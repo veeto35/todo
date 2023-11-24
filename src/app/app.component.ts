1d9e8db8adb5f16c1f6e7321760d0bb63c2889e7
Vasco Poças <killersauros01@gmail.com>
2023-11-24 21:04:34 +0000

    feat: added checkbox for the done section, added a feature to add more to-do task and a toggle to show the completed tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,15 @@ export class AppComponent {
 
 get items(): readonly TodoItem[]{
 
-  return this.list.items;
+  return this.list.items.filter(item => this.showComplete || !item.complete);
 }
+
+addItem(newItem :string){
+  if(newItem != ""){
+    this.list.addItem(newItem);
+  }
+}
+
+showComplete: boolean =false;
 
 }
